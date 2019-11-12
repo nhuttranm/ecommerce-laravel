b6590e19bc3741afc8805eacbb935a52162cf4f9
nhut-gnt <nhut.tm@gnt.com.vn>
2019-11-12 10:15:43 +0700

    No. 5 & 6: Backend Admin Authentication & Base Controller and Repository

--- routes/web.php
+++ routes/web.php
@@ -10,9 +10,11 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+Auth::routes();
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::view('/admin', 'admin.dashboard.index');
+Route::view('/admin/login', 'admin.auth.login');
+require 'admin.php';
